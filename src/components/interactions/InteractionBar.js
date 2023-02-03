4f402a8a78038aa7feb51e0ed7b613c50f987279
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-02-03 14:26:30 +0300

    added interaction bar

--- src/components/interactions/InteractionBar.js
+++ src/components/interactions/InteractionBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function InteractionBar(){
     return(
         <>
-        
+        <div className="flex items-center justify-center">
+  <div className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg" role="group">
+    <button type="button" className="rounded-l inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Like</button>
+    <button type="button" className=" inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Dislike</button>
+    <button type="button" className=" rounded-r inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out">Comment</button>
+  </div>
+</div>
         </>
     )
 }
--- src/components/posts/NewPost.js
+++ src/components/posts/NewPost.js
@@ -3,7 +3,30 @@ import React from "react";
 function NewPost(){
     return(
         <>
-        
+<form>
+  <div className="bg-indigo-50 min-h-screen md:px-20 pt-6">
+    <div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+      <h1 className="text-center text-2xl font-bold text-gray-500 mb-10">ADD POST</h1>
+      <div className="space-y-4">
+        <div>
+        </div>
+        <div>
+          <label for="description" className="block mb-2 text-lg font-serif">Description:</label>
+          <textarea id="description" cols="30" rows="10" placeholder="whrite here.." className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"></textarea>
+        </div>
+        <div>
+          <label for="location" className="text-lx font-serif">Location:</label>
+          <input type="text" placeholder="location" id="location" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
+        </div>
+        <div>
+          <label for="image" className="text-lx font-serif">image:</label>
+          <input type="url" placeholder="url" id="image" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
+        </div>
+        <button className=" px-6 py-2 mx-auto block rounded-md text-lg font-semibold text-indigo-100 bg-indigo-600  ">POST</button>
+      </div>
+    </div>
+  </div>
+</form>
         </>
     )
 }
